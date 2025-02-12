fd62880392651df0e7e3198550fa4b67af5304f3
Amanda Cardoso <amanda.cardoso@sislogica.com.br>
2025-02-12 19:54:33 -0300

    limite

--- antecipacao_recebivel/Models/LimiteAntecipacao.cs
+++ antecipacao_recebivel/Models/LimiteAntecipacao.cs
@@ -6,6 +6,6 @@
         public decimal faixaMin { get; set; }
         public decimal faixaMax { get; set; }
         public int porcentagem { get; set; }
-        public int ramo { get; set; }
+        public int servico { get; set; }
     }
 }
